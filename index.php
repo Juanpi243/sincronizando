5e25c3db3ae16a340f8797f6862a816f8c721a0c
JuanPablo <noelia@gmail.com>
2020-07-26 18:15:32 -0500

    modificando localmente el archivo index

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-<? php
-
- echo "solucionando conflictos"
-
-?>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
->>>>>>> actualizando
+<h1>Mi título principal</h1>
+<h2>Mi título de nivel superior</h2>
+<h3>Mi título</h3>
+<h5>Mi sub-titulo</h5>
